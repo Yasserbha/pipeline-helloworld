528be3d29c09edacbcaf8d3ea42e45fdeed5f4dc
Yasserbha <yasser.belhajali@esprit.tn>
2020-07-27 19:54:29 +0100

    final commit

--- src/helloword/Helloword.java
+++ src/helloword/Helloword.java
@@ -15,7 +15,7 @@ public class Helloword {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.err.println("hi yasser");
+        System.out.println("hi yasser");
     }
     
 }
